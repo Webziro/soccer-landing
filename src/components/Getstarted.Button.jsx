1e0e7cdf6e75a9dea87ab10d5fec6f788f5f8f27
Stanley Amaziro <stanleyamaziro@gmail.com>
2025-10-22 23:18:52 +0100

    Edit

--- src/components/Getstarted.Button.jsx
+++ src/components/Getstarted.Button.jsx
@@ -1,7 +1,7 @@
 function Button({ label, onClick }) {
   return (
      <button style={{ background: '#b18d70', color: 'white', border: 'none', borderRadius: 20, padding: '10px 20px', cursor: 'pointer' }}>
-          Get Started
+          Get Started Now
         </button>
   );
 }
