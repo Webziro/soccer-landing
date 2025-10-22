9c5938bfff10172eafada133977e74daec4b075a
Stanley Amaziro <stanleyamaziro@gmail.com>
2025-10-22 23:19:25 +0100

    Edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer style={{ background: '#111', color: '#ffff', padding: 50, fontSize: 10, textAlign: 'center' }}>
-      <p style={{ margin: 0 }}>Developed by Stanley Aamziro © 2023 CR7 Profile. All rights reserved.</p>
+      <p style={{ margin: 0 }}>Designed by Stanley Aamziro © 2023 CR7 Profile. All rights reserved.</p>
     </footer>
   );
 }
